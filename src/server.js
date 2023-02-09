7939a95d4aea2e48fcb1c61a65e944848e20b11a
LongNguyen15102002 <lavalshy2002@gmail.com>
2023-02-09 17:45:37 +0700

    Fifth Lesson

--- src/server.js
+++ src/server.js
@@ -1,19 +1,17 @@
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
-require('dotenv').config();
+import initWebRoute from "./route/web";
+
+require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
+//Setup View engine
 configViewEngine(app);
 
-app.get("/", (req, res) => {
-  res.render('test/index.ejs')
-});
-
-app.get("/about", (req, res) => {
-  res.send("Hello! ");
-});
+//Init web route
+initWebRoute(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
